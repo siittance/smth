262de30539e49fe70cbe8b6a6206d2123e274b1c
siittance <144900566+siittance@users.noreply.github.com>
2024-01-14 01:40:34 +0300

    j

---
+++ Stih.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include<string>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Любви, надежды, тихой славы\nНедолго нежил нас обман,\nИсчезли юные забавы,\nКак сон, как утренний туман;\n";
+
+    return 0;
+}
